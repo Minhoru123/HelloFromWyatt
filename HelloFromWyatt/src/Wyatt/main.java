e4fb75d5acecd416ec83eb110deb8b24aad87b41
JarenBrown <59661913+JarenBrown@users.noreply.github.com>
2020-01-31 12:28:37 -0700

    Hello

--- HelloFromWyatt/src/Wyatt/main.java
+++ HelloFromWyatt/src/Wyatt/main.java
@@ -3,6 +3,7 @@ package Wyatt;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello From Wyatt.");
-        System.out.println("Hello From Marty Poo.")
+        System.out.println("Hello From Marty Poo.");
+        System.out.println("Hello from Jaren.");
     }
 }
