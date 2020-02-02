b2cc277fb2b13cfc12d51a5e96948bcb20d6e894
Minhoru <mijay_1998@hotmail.com>
2020-02-01 17:14:16 -0700

    HelloFromMinhoru

--- HelloFromWyatt/src/Wyatt/main.java
+++ HelloFromWyatt/src/Wyatt/main.java
@@ -5,5 +5,6 @@ public class main {
         System.out.println("Hello From Wyatt.");
         System.out.println("Hello From Marty Poo.");
         System.out.println("Hello from Jaren.");
+        System.out.println("Hello from Minhoru");
     }
 }
